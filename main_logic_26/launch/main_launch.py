317e174ba17d088a837a2c8aeb20fcdc0607b238
Philippe <pleblond20@gmail.com>
2026-07-02 17:22:09 -0400

    adding display_processing node to launch file and setup.py

---
+++ main_logic_26/launch/main_launch.py
@@ -0,0 +1,63 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='main_logic_26',
+            executable='sensors_reading',
+            name='sensors_reading_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='line_processing',
+            name='line_processing_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='ultrasonic_processing',
+            name='ultrasonic_processing_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='motion_controller',
+            name='motion_controller_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='state_machine',
+            name='state_machine_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='camera_processing',
+            name='camera_processing_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='led_processing',
+            name='led_processing_node',
+            output='screen'
+        ),
+
+        Node(
+            package='main_logic_26',
+            executable='display_processing',
+            name='display_processing_node',
+            output='screen'
+        ),
+    ])
--- main_logic_26/setup.py
+++ main_logic_26/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'main_logic_26'
 
@@ -10,11 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/main_launch.py']),
+        (os.path.join('share', 'main_logic_26', 'config'),
+            glob('config/colorrange.json')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='corn',
-    maintainer_email='corn@todo.todo',
+    maintainer='phil',
+    maintainer_email='pleblond20@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
@@ -24,6 +30,14 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'sensors_reading = main_logic_26.sensors_reading:main',
+            'line_processing = main_logic_26.line_processing:main',
+            'ultrasonic_processing = main_logic_26.ultrasonic_processing:main',
+            'motion_controller = main_logic_26.motion_controller:main',
+            'state_machine = main_logic_26.state_machine:main',
+            'camera_processing = main_logic_26.camera_processing:main',
+            'led_processing = main_logic_26.led_processing:main',
+            'display_processing = main_logic_26.display_processing:main',
         ],
     },
 )
